47c5b575218d4abe6e4fee3cb46792274e26f4ff
Sandiso Njadu <Sandiso.Njadu@capaciti.org.za>
2024-10-11 12:54:53 +0200

    added dummy logins for live demo visitors

--- data.js
+++ data.js
@@ -1,17 +1,5 @@
 let workers = [
-    {usr : "sandiso@agile5", 
-      psd: "agile5@capaciti"
-    },
-    {usr : "athini@agile5", 
-      psd: "agile5@capaciti"
-    },
-    {usr : "amahle@agile5", 
-      psd: "agile5@capaciti"
-    },
-    {usr : "vice@agile5", 
-      psd: "agile5@capaciti"
-    },
-    {usr : "cibrian@agile5", 
+    {usr : "worker1", 
       psd: "agile5@capaciti"
     }
 ]
